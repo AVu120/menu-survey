cccaa809c1366ad2629328adfe635b8acc0824c6
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-08-22 07:17:41 +1000

    Add successful review submit message and button to return to menu after user submits review.

--- src/pages/survey/Survey.tsx
+++ src/pages/survey/Survey.tsx
@@ -3,10 +3,13 @@ import Emoticon from "../../components/survey/emoticon/Emoticon";
 import StarRatingScale from "../../components/survey/starRatingScale/StarRatingScale";
 import { IMap } from "../../types/interfaces";
 import styles from "./Survey.module.css";
+import { useHistory } from "react-router";
 
 const Survey = () => {
   const [hoverRating, setHoverRating] = useState(0);
   const [selectedRating, setSelectedRating] = useState(0);
+  const [isFeedbackSubmitted, setIsFeedbackSubmitted] = useState(false);
+  const history = useHistory();
 
   const changeRating = (rating: number) => {
     setSelectedRating(rating);
@@ -19,6 +22,11 @@ const Survey = () => {
   const hoverOffRating = (rating: number) => {
     setHoverRating(rating);
   };
+
+  const changeIsFeedbackSubmitted = (): void =>
+    setIsFeedbackSubmitted(!isFeedbackSubmitted);
+
+  const returnToMenu = (): void => history.push("/");
 
   const ratingToBackgroundMap: IMap = {
     1: "shock",
@@ -38,23 +46,47 @@ const Survey = () => {
         selectedEmoticon={ratingToBackgroundMap[selectedRating] || "smile"}
       />
 
-      <div className={styles.rate_meal_label}>Rate your meal</div>
-      <StarRatingScale
-        starCount={5}
-        hoverRating={hoverRating}
-        selectedRating={selectedRating}
-        onClick={changeRating}
-        onMouseOver={hoverOverRating}
-        onMouseLeave={hoverOffRating}
-      />
-      {selectedRating > 0 && (
+      {!isFeedbackSubmitted ? (
         <>
-          <div className={styles.comment_input_label}>
-            Leave a comment (optional)?
+          <div className={styles.rate_meal_label}>Rate your meal</div>
+          <StarRatingScale
+            starCount={5}
+            hoverRating={hoverRating}
+            selectedRating={selectedRating}
+            onClick={changeRating}
+            onMouseOver={hoverOverRating}
+            onMouseLeave={hoverOffRating}
+          />
+          {selectedRating > 0 && (
+            <>
+              <div className={styles.comment_input_label}>
+                Leave a comment (optional)?
+              </div>
+              <textarea rows={4} cols={50} className={styles.comment_input} />
+              <button
+                type="button"
+                className={styles.button}
+                onClick={changeIsFeedbackSubmitted}
+              >
+                Submit feedback
+              </button>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <div className={styles.survey_submitted_label}>
+            Thank you!
+            <br />
+            <br />
+            Your feedback has been submitted.
           </div>
-          <textarea rows={4} cols={50} className={styles.comment_input} />
-          <button type="button" className={styles.submit_button}>
-            Submit feedback
+          <button
+            type="button"
+            className={styles.button}
+            onClick={returnToMenu}
+          >
+            Return to menu
           </button>
         </>
       )}
